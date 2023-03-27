cdefa068d7d9a145df4ffd0309d520d20295f671
Jaesung <jaesung.lee@sendbird.com>
2023-03-28 00:24:32 +0900

    [FIX] Fixed error

--- src/routes/.well-known/apple-app-site-association/+page.server.ts
+++ src/routes/.well-known/apple-app-site-association/+page.server.ts
@@ -1,5 +1,5 @@
 /** @type {import('./$types').PageLoad} */
-export function load() {
+export async function load() {
     const response = await this.fetch("/apple-app-site-association.json");
     const jsonRes = await response.json();
     return jsonRes;
